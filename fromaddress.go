c322ffd8cbce04cbbea0eedf57f52ca92891ddd2
MrMelon54 <github@mrmelon54.com>
2025-01-10 23:52:43 +0000

    Rework FromAddress to prevent all the nesting

--- fromaddress.go
+++ fromaddress.go
@@ -5,17 +5,24 @@ import (
 	"github.com/emersion/go-message/mail"
 )
 
-type FromAddress struct {
-	*mail.Address
-}
+type FromAddress mail.Address
 
 var _ encoding.TextUnmarshaler = &FromAddress{}
+
+func (f FromAddress) String() string {
+	return f.toMailAddress().String()
+}
 
 func (f *FromAddress) UnmarshalText(b []byte) error {
 	address, err := mail.ParseAddress(string(b))
 	if err != nil {
 		return err
 	}
-	f.Address = address
+	*f = FromAddress(*address)
 	return nil
+}
+
+func (f FromAddress) toMailAddress() *mail.Address {
+	m := mail.Address(f)
+	return &m
 }
--- fromaddress_test.go
+++ fromaddress_test.go
@@ -11,13 +11,13 @@ func TestFromAddress_UnmarshalText(t *testing.T) {
 	t.Run("json", func(t *testing.T) {
 		var fromAddress FromAddress
 		assert.NoError(t, json.Unmarshal([]byte(`"Jane Doe <jane@example.com>"`), &fromAddress))
-		assert.Equal(t, "Jane Doe", fromAddress.Address.Name)
-		assert.Equal(t, "jane@example.com", fromAddress.Address.Address)
+		assert.Equal(t, "Jane Doe", fromAddress.Name)
+		assert.Equal(t, "jane@example.com", fromAddress.Address)
 	})
 	t.Run("yaml", func(t *testing.T) {
 		var fromAddress FromAddress
 		assert.NoError(t, yaml.Unmarshal([]byte(`"Jane Doe <jane@example.com>"`), &fromAddress))
-		assert.Equal(t, "Jane Doe", fromAddress.Address.Name)
-		assert.Equal(t, "jane@example.com", fromAddress.Address.Address)
+		assert.Equal(t, "Jane Doe", fromAddress.Name)
+		assert.Equal(t, "jane@example.com", fromAddress.Address)
 	})
 }
